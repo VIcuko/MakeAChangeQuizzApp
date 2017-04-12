596f23614aba11edc9c0863ea1709e7b5fe56ebb
Vicuko <victorc.fernandez@gmail.com>
2017-04-13 00:02:13 +0200

    last hour fixes

--- app/src/main/java/com/example/android/makeachangequizzapp/MainActivity.java
+++ app/src/main/java/com/example/android/makeachangequizzapp/MainActivity.java
@@ -1,10 +1,25 @@
 package com.example.android.makeachangequizzapp;
 
+import android.content.Intent;
+import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+import static android.os.Build.VERSION_CODES.M;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
             "to achieve your objective!"};
 
 
-    String[] questionAnswers;
+    ArrayList answersContent;
 
     TextView questionNumberView;
     TextView questionTextView;
@@ -148,7 +163,85 @@ public class MainActivity extends AppCompatActivity {
         return buttonId;
     };
 
-    public void finishQuiz(String[] questionAnswers) {
+    public void addToCalendar(View view) {
+        ArrayList userAnswers = getUserAnswers();
+        addEventToCalendar(userAnswers);
+    };
 
+    public ArrayList getUserAnswers (){
+        ArrayList userAnswers = new ArrayList();
+
+        String answer1 = ((EditText) findViewById(R.id.answer_1_text)).getText().toString();
+        userAnswers.add(answer1);
+
+        String answer2 = ((EditText) findViewById(R.id.answer_2_text)).getText().toString();
+        userAnswers.add(answer2);
+
+        int selectedRadioButton = ((RadioGroup) findViewById(R.id.answer_3)).getCheckedRadioButtonId();
+        String answer3 = ((RadioButton) findViewById(selectedRadioButton)).getText().toString();
+        userAnswers.add(answer3.split("")[0]);
+
+        int answer4 = 0;
+        String weekdayContent;
+        for (int i=1;i<=7;i++){
+            weekdayContent = "weekday_"+i;
+            int weekdayId = getResources().getIdentifier(weekdayContent, "id", this.getPackageName());
+            if (((CheckBox) findViewById(weekdayId)).isChecked()){
+                answer4+=1;
+            }
+        }
+        userAnswers.add(answer4);
+
+        ArrayList answer5 = new ArrayList();
+        int answer5_hours = ((TimePicker) findViewById(R.id.answer_5)).getHour();
+        answer5.add(answer5_hours);
+
+        int answer5_minutes = ((TimePicker) findViewById(R.id.answer_5)).getMinute();
+        answer5.add(answer5_minutes);
+        SimpleDateFormat answer5b;
+        //        String answer5 = String.format("%02d%02d", answer5_hours, answer5_minutes);
+
+        userAnswers.add(answer5);
+
+        String answer6 = ((EditText) findViewById(R.id.answer_6)).getText().toString();
+        userAnswers.add(answer6);
+
+//        ArrayList answer7 = new ArrayList();
+        int answer7_day = ((DatePicker) findViewById(R.id.answer_7)).getDayOfMonth();
+//        answer7.add(answer7_day);
+
+        int answer7_month = ((DatePicker) findViewById(R.id.answer_7)).getMonth();
+//        answer7.add(answer7_month);
+
+        int answer7_year = ((DatePicker) findViewById(R.id.answer_7)).getYear();
+//        answer7.add(answer7_year);
+        String answer7 = answer7_day+"-"+answer7_month+"-"+answer7_year;
+
+        userAnswers.add(answer7);
+
+        return userAnswers;
+    };
+
+    public void addEventToCalendar(ArrayList userAnswers){
+        Calendar c = Calendar.getInstance();
+        System.out.println("Current time => " + c.getTime());
+
+        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+        String formattedDate = df.format(c.getTime());
+
+        Intent intent = new Intent(Intent.ACTION_INSERT)
+                .setData(CalendarContract.Events.CONTENT_URI)
+                .putExtra(CalendarContract.Events.RRULE,"FREQ=DAILY;INTERVAL="+(7-Integer.parseInt(userAnswers.get(3).toString()))+";UNTIL="+userAnswers.get(6).toString())
+                .putExtra(CalendarContract.Events.TITLE, userAnswers.get(0).toString())
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, formattedDate)
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, userAnswers.get(6).toString())
+                .putExtra(CalendarContract.Events.HAS_ALARM,true)
+                .putExtra(CalendarContract.Events.DURATION,userAnswers.get(2).toString())
+                .putExtra(CalendarContract.Events.DESCRIPTION,userAnswers.get(1).toString()+"."+userAnswers.get(5).toString());
+
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     };
 };
